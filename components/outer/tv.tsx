d20f2b3b1f9ada109a77c09e16ae0d260f909d82
Jace <jacealan1@gmail.com>
2024-05-28 12:27:14 +0900

    fix: hide no link tv
    Closes

--- components/outer/tv.tsx
+++ components/outer/tv.tsx
@@ -68,18 +68,23 @@ export default function Tv() {
         padding={2}
       >
         {gstvData &&
-          gstvData.map((gstv: any, index: number) => (
-            <GridItem key={index}>
-              <div className="video">
-                <iframe
-                  src={`https://www.youtube.com/embed/${youtubeId(gstv.link)}`}
-                  title="YouTube video player"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                  allowFullScreen
-                ></iframe>
-              </div>
-            </GridItem>
-          ))}
+          gstvData.map(
+            (gstv: any, index: number) =>
+              gstv.link !== "" && (
+                <GridItem key={index}>
+                  <div className="video">
+                    <iframe
+                      src={`https://www.youtube.com/embed/${youtubeId(
+                        gstv.link
+                      )}`}
+                      title="YouTube video player"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                      allowFullScreen
+                    ></iframe>
+                  </div>
+                </GridItem>
+              )
+          )}
       </Grid>
 
       <style jsx>{`
