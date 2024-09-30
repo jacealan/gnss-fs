bef080dfa4eac3f5042caeb139fdf08e23b9d655
Jace@MacStudio <jacealan1@gmail.com>
2024-09-30 20:16:47 +0900

    modify event/PlDt

--- components/outer/event/PlDt.tsx
+++ components/outer/event/PlDt.tsx
@@ -64,15 +64,15 @@ export default function EventPlDt() {
       </Box>
       <Box width="100%">
         <Image src="/assets/events/event6.png"></Image>
-        <Center flexDirection={"column"}>
-          <Link
-            href="https://map.naver.com/v5/search/%EA%B2%BD%EA%B8%B0%EB%8F%84%20%ED%99%94%EC%84%B1%EC%8B%9C%20%EB%8F%99%ED%83%84%EC%88%9C%ED%99%98%EB%8C%80%EB%A1%9C%20127-19%20%EB%8F%99%ED%83%84%EC%97%90%EC%8A%A4%EB%B9%84%ED%83%80%EC%9A%B4%208%EC%B8%B5"
-            target="_blank"
-          >
+        <Link
+          href="https://map.naver.com/v5/search/%EA%B2%BD%EA%B8%B0%EB%8F%84%20%ED%99%94%EC%84%B1%EC%8B%9C%20%EB%8F%99%ED%83%84%EC%88%9C%ED%99%98%EB%8C%80%EB%A1%9C%20127-19%20%EB%8F%99%ED%83%84%EC%97%90%EC%8A%A4%EB%B9%84%ED%83%80%EC%9A%B4%208%EC%B8%B5"
+          target="_blank"
+        >
+          <Center flexDirection={"column"}>
             <img
               className="map"
               src="http://t1.daumcdn.net/roughmap/imgmap/b7cd7edb8ecaa98e9f60b9c3ec0b67fe9a4bdde73a4843ad0888584fc10d2848"
-              // width="638px"
+              width="90%"
               // height="358px"
             />
             <Flex
@@ -86,16 +86,24 @@ export default function EventPlDt() {
               <FaBusAlt />
               <Box>&nbsp;&nbsp;남동탄지역 셔틀버스 운영예정</Box>
             </Flex>
-          </Link>
-          <br />
-          <br />
-        </Center>
+            <br />
+            <br />
+          </Center>
+        </Link>
       </Box>
       <Box>
         <Link href="https://forms.gle/XhHZ9J956Xmoo41R8" target="_blank">
           <Image src="/assets/events/event3.png"></Image>
         </Link>
       </Box>
+      <Center
+        fontSize={vw && (50 / (760 / vwC)).toString() + "px"}
+        fontFamily={"GmarketSans"}
+        fontWeight={900}
+        p={6}
+      >
+        <Link href={`tell://${branch?.phone}`}>상담문의: 031. 375. 4808</Link>
+      </Center>
     </>
   )
 }
