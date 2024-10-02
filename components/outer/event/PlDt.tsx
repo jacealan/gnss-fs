4ce96b43d2b132c7e41774f537a78d958835477e
Jace@MacStudio <jacealan1@gmail.com>
2024-10-02 14:12:51 +0900

    use google map

--- components/outer/event/PlDt.tsx
+++ components/outer/event/PlDt.tsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react"
 import { useRouter } from "next/router"
 import Head from "next/head"
 import Link from "next/link"
+import Script from "next/script"
+import { Map, MapMarker } from "react-kakao-maps-sdk"
 
 import { useRealViewport } from "next-real-viewport"
 import { Flex, Center, Box, Image } from "@chakra-ui/react"
@@ -10,6 +12,7 @@ import getBranch from "@/lib/getBranch"
 import { HiChevronRight } from "react-icons/hi2"
 import { TbPhoneCall } from "react-icons/tb"
 import { FaBusAlt } from "react-icons/fa"
+import { NextScript } from "next/document"
 
 export default function EventPlDt() {
   const { vw, vh } = useRealViewport()
@@ -69,12 +72,31 @@ export default function EventPlDt() {
           target="_blank"
         >
           <Center flexDirection={"column"}>
-            <img
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7352.030879395358!2d127.09796545032377!3d37.166264161838264!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357b47d1301f60db%3A0x2ce4450a86264f5b!2z7JeQ7Iqk67mE7YOA7Jq0!5e0!3m2!1sko!2skr!4v1727845620427!5m2!1sko!2skr"
+              width="100%"
+              height={(vwC * 9) / 16}
+              // style="border:0;"
+              // allowfullscreen=""
+              loading="lazy"
+              // referrerpolicy="no-referrer-when-downgrade"
+            ></iframe>
+            {/* <Map
+              center={{ lat: 37.167816, lng: 127.10717 }}
+              style={{ width: "100%", height: "360px" }}
+              level={5}
+            >
+              <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
+                <div style={{ color: "#000" }}>개념폴리아 동탄관</div>
+              </MapMarker>
+            </Map> */}
+
+            {/* <img
               className="map"
               src="http://t1.daumcdn.net/roughmap/imgmap/b7cd7edb8ecaa98e9f60b9c3ec0b67fe9a4bdde73a4843ad0888584fc10d2848"
               width="90%"
               // height="358px"
-            />
+            /> */}
             <Flex
               width="90%"
               fontSize={vw && (20 / (760 / vwC)).toString() + "px"}
