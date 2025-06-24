e89edb92baa0ca67538c181c9efe353ed4317c47
Jace@MacStudio <jacealan1@gmail.com>
2025-06-24 13:10:20 +0900

    test9 width

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,10 @@ import Script from "next/script"
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head />
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
