8a62a7bc3e81f7d7f0cf5a53fb0bd3a51bfbce9c
Jace@MacStudio <jacealan1@gmail.com>
2025-11-07 17:59:15 +0900

    add link PlSd/board

--- components/outer/slideshow.tsx
+++ components/outer/slideshow.tsx
@@ -11,7 +11,7 @@ const spanStyle = {
 }
 
 const slideLists = [
-  { imageNum: 3, link: "" },
+  { imageNum: 3, link: "https://gnss.co.kr/branch/PlSd/board" },
   // { imageNum: 0, link: "https://blog.naver.com/gnss0508/224004497472" },
   { imageNum: 1, link: "https://blog.naver.com/gnsc2505/224044542722" },
   { imageNum: 2, link: "https://blog.naver.com/gnss0508" },
