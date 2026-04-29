bc31637b13276e49e29d95f7a565a5da0a07cffa
Jace@MacStudio <jacealan1@gmail.com>
2026-04-29 13:55:39 +0900

    add banner

--- components/outer/slideshow.tsx
+++ components/outer/slideshow.tsx
@@ -12,8 +12,8 @@ const spanStyle = {
 
 const slideLists = [
   { imageNum: 4, link: "https://blog.naver.com/gnpolya23/224253575597" },
+  { imageNum: 2, link: "https://blog.naver.com/gnss0508" },
   { imageNum: 0, link: "https://blog.naver.com/gnsc2505/224249573220" },
-  // { imageNum: 2, link: "https://m.blog.naver.com/gnss0508/224156859041" },
   // { imageNum: 3, link: "https://blog.naver.com/gnsc2505/224153816082" },
   // { imageNum: 1, link: "" },
   // { imageNum: 3, link: "https://gnss.co.kr/branch/PlSd/board" },
